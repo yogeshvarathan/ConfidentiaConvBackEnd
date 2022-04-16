fad966fa0a5e6c09e923050450eb18cbb6fa7f26
YogeshVarathan <yogeshwaranvarathan@gmail.com>
2022-04-16 12:59:57 -0600

    Initial Service Definition

---
+++ src/UserProfileService/src/test/java/com/igic/confidentiaconv/UserProfileService/UserProfileServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.igic.confidentiaconv.UserProfileService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void validateUserProfie()
+
+}
