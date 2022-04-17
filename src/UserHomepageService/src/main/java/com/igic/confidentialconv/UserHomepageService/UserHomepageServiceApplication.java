78198066a454881b751d4ad738ecee0e26e612dc
YogeshVarathan <yogeshwaranvarathan@gmail.com>
2022-04-17 16:08:05 -0600

    Initial Service Definition for homepage service

---
+++ src/UserHomepageService/src/main/java/com/igic/confidentialconv/UserHomepageService/UserHomepageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.igic.confidentialconv.UserHomepageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHomepageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHomepageServiceApplication.class, args);
+	}
+
+}
---
+++ src/UserHomepageService/src/test/java/com/igic/confidentialconv/UserHomepageService/UserHomepageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igic.confidentialconv.UserHomepageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHomepageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
